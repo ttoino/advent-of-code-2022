d706a957b8c17b155ead1779eb2374710fb3fb17
toino <joaoapereira21@hotmail.com>
2022-12-02 23:35:38 +0000

    Add solutions to the day 1 puzzles

---
+++ day01/part2.py
@@ -0,0 +1,11 @@
+import itertools as it
+import more_itertools as mit
+import functools as ft
+import operator as op
+import re
+from collections import Counter
+
+with open("input") as inf, open("part2.out", "w+") as outf:
+    elves = [sum(map(int, e.splitlines())) for e in inf.read().split("\n\n")]
+
+    outf.write(str(sum(sorted(elves)[-3:])))
